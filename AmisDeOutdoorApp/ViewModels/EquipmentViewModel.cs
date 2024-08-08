df1a06edb1602009ca49a0673728cb3c9b6f9297
SJ <serge.szynkarski@le-campus-numerique.fr>
2024-08-08 14:44:12 +0200

    Added Equipment functions

--- AmisDeOutdoorApp/ViewModels/EquipmentViewModel.cs
+++ AmisDeOutdoorApp/ViewModels/EquipmentViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using AmisDeOutdoorApp.Models;
 
 namespace AmisDeOutdoorApp.ViewModels
 {
-    internal class EquipmentViewModel
+    /// <summary>
+    /// ViewModel for managing a list of hiking equipment.
+    /// </summary>
+    public class EquipmentViewModel
     {
+        /// <summary>
+        /// Gets the list of equipment.
+        /// </summary>
+        public ObservableCollection<Equipment> EquipmentList { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EquipmentViewModel"/> class with sample data.
+        /// </summary>
+        public EquipmentViewModel()
+        {
+            EquipmentList = new ObservableCollection<Equipment>
+            {
+                new Equipment { Name = "Boots", Available = 1, Max = 4 },
+                new Equipment { Name = "Backpack", Available = 2, Max = 5 },
+                new Equipment { Name = "Tent", Available = 1, Max = 2 },
+                new Equipment { Name = "Sleeping Bag", Available = 3, Max = 3 },
+                new Equipment { Name = "Map", Available = 0, Max = 2 }
+            };
+        }
     }
 }
